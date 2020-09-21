9fde93ae99419b9c1ce9412c4360ce72d65e17dc
jayakannan.k <jayakannan.karuppasamy@cognizant.com>
2020-09-21 13:18:45 +0530

    loan-management-discoveryService

---
+++ src/test/java/com/scb/loan/management/LoanManagementDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scb.loan.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanManagementDiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
